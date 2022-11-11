573f28885614dd8e1a491748a4e6583b5d38ccde
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-11-11 10:10:28 -0300

    fetching with ssr

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import {client} from '../lib/client'
 import { HeroBanner, Product, FooterBanner } from '../components'
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
+  const banner = bannerData.length && bannerData[0]
   return (
     <>
-      <HeroBanner />
+      <HeroBanner herobanner={banner}/>
       <div className='products-heading'>
         <h2 >Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
         {
-          ['Producto 1', 'Producto 2'].map((product) => product)
+          products?.map(product => product.name)
         }
       </div>
 
@@ -19,6 +21,23 @@ const Home = () => {
       <FooterBanner />
     </>
   )
+}
+
+// aqui hacemos el fetch con SSR
+export const getServerSideProps = async () => {
+  //fetching de productos
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  //fetching de banner
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  // retornamos los props que vienen del servidor
+  return {
+    props : {products, bannerData}
+  }
+
 }
 
 export default Home
